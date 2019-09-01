f47eeedfb8c745313485e8991d276039ebf7cc79
Alex R Rezende <arrezende@gmail.com>
2019-09-01 14:32:56 -0300

    ajustado algumas coisas

--- varreUrl.py
+++ varreUrl.py
@@ -1,12 +1,29 @@
 #html5lib
-from selenium import webdriver
+'''from selenium import webdriver
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
+'''
+
+#LISTAR TODOS OS HEADING TAGS DA PAGINA
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+def url(url,tag):
+    #html = urlopen(url)
+    req = Request(url=url, headers=headers) 
+    tag = tag.split(',')
+    html = urlopen(req).read()
+    bs = BeautifulSoup(html, "html.parser")
+    titles = bs.find_all(tag)
+    print('Resultados Obtidos :', *titles, sep='\n\n')
 
 
-def url(url, tag):
+'''def url(url, tag):
 	
     try:
         html = urlopen(url)
@@ -21,3 +38,4 @@ def url(url, tag):
         for tag in tags:
             #print(tag.getText())
             print(tag)
+'''
---
+++ visual.py
@@ -0,0 +1,8 @@
+import tkinter
+
+janela = tkinter.Tk()
+janela.title('Coisas Úteis')
+
+janela['bg'] = 'green'
+janela.geometry('800x600')
+janela.mainloop()
